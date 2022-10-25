826c423c9d057e5b8b6e0be72d9dfdd369554eab
Angelika Stano <archiangel809@gmail.com>
2022-10-25 23:50:04 +0200

    arrow functions added

--- js/script.js
+++ js/script.js
@@ -1,54 +1,67 @@
-let formElement = document.querySelector(".js-form");
-let ammountElement = document.querySelector(".js-ammount");
-let currencyElement = document.querySelector(".js-currency");
-let resultElement = document.querySelector(".js-result");
+{
+    const calculateResult = (ammount, currency) => {
+        const rateEUR = 4.8330;
+        const rateUSD = 4.9569;
+        const rateCHF = 4.9526;
+        const rateGBP = 5.5886;
+        const rateCZK = 0.1965;
+        const rateHRK = 0.6415;
+        const rateJPY = 3.3561;
 
-let rateEUR = 4.8330;
-let rateUSD = 4.9569;
-let rateCHF = 4.9526;
-let rateGBP = 5.5886;
-let rateCZK = 0.1965;
-let rateHRK = 0.6415;
-let rateJPY = 3.3561;
+        switch (currency) {
+            case "EUR":
+                return ammount / rateEUR;
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
 
-    let currency = currencyElement.value;
-    let ammount = +ammountElement.value;
+            case "USD":
+                return ammount / rateUSD;
 
-    let result;
 
-    switch (currency) {
-        case "EUR":
-            result = ammount / rateEUR;
-            break;
+            case "CHF":
+                return ammount / rateCHF;
 
-        case "USD":
-            result = ammount / rateUSD;
-            break;
 
-        case "CHF":
-            result = ammount / rateCHF;
-            break;
+            case "GBP":
+                return ammount / rateGBP;
 
-        case "GBP":
-            result = ammount / rateGBP;
-            break;
 
-        case "CZK":
-            result = ammount / rateCZK;
-            break;
+            case "CZK":
+                return ammount / rateCZK;
 
-        case "HRK":
-            result = ammount / rateHRK;
-            break;
 
-        case "JPY":
-            result = ammount / rateJPY;
-            break;
+            case "HRK":
+                return ammount / rateHRK;
+
+
+            case "JPY":
+                return ammount / rateJPY;
+        }
+    };
+
+    const updateResultText = (ammount, result, currency) => {
+        const resultElement = document.querySelector(".js-result");
+        resultElement.innerHTML = `${ammount.toFixed(2)} PLN = ${result.toFixed(2)} ${currency}`;
     }
 
-    resultElement.innerText = `PLN = ${result.toFixed(2)} ${currency}`;
+    const onFormSubmit = (event) => {
+        event.preventDefault();
 
-});
+        const ammountElement = document.querySelector(".js-ammount");
+        const currencyElement = document.querySelector(".js-currency");
+
+        const currency = currencyElement.value;
+        const ammount = +ammountElement.value;
+
+        let result = calculateResult(ammount, currency);
+
+        updateResultText(ammount, result, currency);
+    };
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+
+        formElement.addEventListener("submit", onFormSubmit);
+    };
+
+    init();
+}
